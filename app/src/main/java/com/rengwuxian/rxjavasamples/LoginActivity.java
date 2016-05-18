3958a461a8e28a09bf1e10ef911c77aa30c8c17c
MuchContact <liuhengjiangis@foxmail.com>
2016-05-18 14:15:06 +0800

    添加登陆界面

---
+++ app/src/main/java/com/rengwuxian/rxjavasamples/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.rengwuxian.rxjavasamples;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.login)
+    public void login(){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
